83127a47866e1ca70833cf4beebc3c22c0a17f14
Jackson8195 <52786680+Jackson8195@users.noreply.github.com>
2025-12-05 17:29:55 -0500

    Update weather.py

--- src/plugins/weather/weather.py
+++ src/plugins/weather/weather.py
@@ -129,10 +129,10 @@ class Weather(BasePlugin):
             last_refresh_time = now.strftime("%Y-%m-%d %I:%M %p")
         template_params["last_refresh_time"] = last_refresh_time
 
-# Add uptime values
-total, since_charge = update_uptime()
-template_params["total_uptime"] = total
-template_params["battery_uptime"] = since_charge
+        # Add uptime values
+        total, since_charge = update_uptime()
+        template_params["total_uptime"] = total
+        template_params["battery_uptime"] = since_charge
 
         image = self.render_image(dimensions, "weather.html", "weather.css", template_params)
 
